7fad3a90d16b4f7a6501965716ec8e055119b849
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2025-05-17 02:06:53 -0400

    Creating models and migrations

---
+++ models/task.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Task.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user'
+      });
+    }
+  }
+
+  Task.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.ENUM('Pendiente', 'En progreso', 'Completada'),
+      allowNull: false,
+      defaultValue: 'Pendiente'
+    },
+    dueDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Task',
+  });
+
+  return Task;
+};
